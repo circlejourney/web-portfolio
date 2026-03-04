9a8e3b32c3208fd36e235fb52000f05852b34d36
circlejourney <sceptile_rayquaza@hotmail.com>
2026-03-04 18:00:03 +1000

    Remove loading animation

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,8 @@
 export default function Loading() {
-  let txt = "";
-  setInterval(()=>{
-    if(txt.length >= 6) txt = ""
-    else txt += ".";
-  }, 500);
+
   return <div className="flex items-center justify-center text-xl uppercase h-screen">
     <span>
-    Loading{txt}
+    Loading...
     </span>
   </div>
 }
